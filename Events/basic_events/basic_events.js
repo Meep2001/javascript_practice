4e50f3d2344658debe6346aba1ef3bd946e5f0d9
anish <nairanish2001@gmail.com>
2022-10-29 12:05:05 +0530

    .

---
+++ Events/basic_events/basic_events.js
@@ -0,0 +1,52 @@
+/*
+    EVENTS : Signal that something has happened
+
+    Mouse Events :
+        click
+        contentmenu - mouse right clicks
+        mouseover/mousedown
+        mousemove  - when the mouse is moved 
+    
+    Keyboard Events :
+        keyup
+        keydown
+
+    Form Element Events :
+        submit
+        focus
+    
+    Document Events :
+        DOMContentLoaded
+    
+    CSS events :
+        transitioned
+      
+*/
+
+const event_name = document.getElementById("event_name");
+const event_element = document.getElementById("event_emmiter_element");
+
+event_element.addEventListener("click", (e) => {
+  event_name.innerHTML = "CLICK";
+});
+event_element.addEventListener("mouseover", (e) => {
+  event_name.innerHTML = "MOUSE OVER";
+});
+
+event_element.addEventListener("mouseout", (e) => {
+  event_name.innerHTML = "MOUSE OUT";
+});
+event_element.addEventListener("mousedown", (e) => {
+  event_name.innerHTML = "MOUSE DOWN";
+});
+event_element.addEventListener("mouseup", (e) => {
+  event_name.innerHTML = "MOUSE UP";
+});
+event_element.addEventListener("mousemove", (e) => {
+  event_name.innerHTML = "MOUSE MOVE";
+});
+
+//Not supported for divs -  no action here
+event_element.addEventListener("keydown", (e) => {
+  event_name.innerHTML = "KEY DOWN";
+});
